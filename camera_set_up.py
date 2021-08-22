912ab414101468c5f0dd61d47875f039a6e45a53
Kieron <17520284+KieronJenkins@users.noreply.github.com>
2021-08-22 19:14:58 +0100

    Update camera_set_up.py

--- camera_set_up.py
+++ camera_set_up.py
@@ -3,10 +3,10 @@ import maya.cmds as cmds
 # Made using Python. This script changes the four main default Maya camera near and far clip planes so that they don't clip and deletes history.
 
 def maya_camera_clipping():
-    cmds.viewClipPlane( "frontShape", ncp="1.0", fcp = "100000.0" ) # To change the clipping planes, change the numbers inbetween the " " to what you need
-    cmds.viewClipPlane( "perspShape", ncp="1.0", fcp = "100000.0" ) # ncp = near clip plane // fcp = far clip plane
-    cmds.viewClipPlane( "sideShape", ncp="1.0", fcp = "100000.0" )
-    cmds.viewClipPlane( "topShape", ncp="1.0", fcp = "100000.0" )
+    cmds.viewClipPlane( "frontShape", ncp="0.1", fcp = "100000.0" ) # To change the clipping planes, change the numbers inbetween the " " to what you need
+    cmds.viewClipPlane( "perspShape", ncp="0.1", fcp = "100000.0" ) # ncp = near clip plane // fcp = far clip plane
+    cmds.viewClipPlane( "sideShape", ncp="0.1", fcp = "100000.0" )
+    cmds.viewClipPlane( "topShape", ncp="0.1", fcp = "100000.0" )
     cmds.delete(constructionHistory = True) # Delete this if you don't want to delete history
     print("Cameras Updated") # Delete this if you don't want to print anything in the console afterwards
     
